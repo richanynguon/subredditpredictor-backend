76fb9fec7d2fe574ef16067a3f57ab76746304b5
Melquisedeque Pereira <melqui1@live.com.pt>
2019-11-16 23:17:50 +0000

    Added middleware to handle restricted endpoints and errors

--- services/global/globalHelpers.js
+++ services/global/globalHelpers.js
@@ -1,6 +1,39 @@
 function logger(req, res, next) {
-	console.log(`[${new Date().toISOString()}] ${req.method} to ${req.url}`);
-	next();
+  console.log(`[${new Date().toISOString()}] ${req.method} to ${req.url}`);
+  next();
 }
 
-module.exports = { logger };
+const requireLogin = (req, res, next) => {
+  const token = req.headers.authorization;
+  if (token) {
+    jwt.verify(token, process.env.TOKENSECRET, (err, decodedUser) => {
+      if (err) {
+        next({ message: err });
+      } else {
+        req.loggedInUser = decodedUser;
+        next();
+      }
+    });
+  } else {
+    next({ status: 403, message: "You need to login in order to gain access!" });
+  }
+}
+
+const handleErrors = (file, router) => {
+  router.use((error, req, res, next) => {
+    res.status(error.status || 500).json({
+      file: file,
+      url: req.url,
+      method: req.method,
+      status: error.status || 500,
+      message: error.message,
+      protocol: req.protocol
+    }).end();
+  });
+}
+
+module.exports = {
+  logger,
+  requireLogin,
+  handleErrors
+};
