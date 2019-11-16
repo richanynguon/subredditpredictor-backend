8b4d77daa2f1f11495305c7fec8a27e85fb86731
Melquisedeque Pereira <melqui1@live.com.pt>
2019-11-16 23:16:55 +0000

    Added auth router to server

--- api/server.js
+++ api/server.js
@@ -1,10 +1,9 @@
-require("dotenv").config();
-
 const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
 
 const { logger } = require("../services/global/globalHelpers");
+const authRouter = require('../services/auth/authRouter');
 
 const server = express();
 
@@ -20,5 +19,7 @@ server.use((req, res, next) => {
 server.get("/", (req, res) => {
 	res.status(200).json({ message: "Server is running" });
 });
+
+server.use("/api/auth", authRouter);
 
 module.exports = server;
