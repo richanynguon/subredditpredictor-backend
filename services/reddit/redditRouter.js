68702065d6201669d49a5322c2f18f700810fae3
Melquisedeque Pereira <melqui1@live.com.pt>
2019-11-18 17:05:34 +0000

    Added required packages, fixed bcrypt compare

--- services/reddit/redditRouter.js
+++ services/reddit/redditRouter.js
@@ -1,3 +1,5 @@
+const config = require('../../config');
+const bcrypt = require('bcryptjs');
 const redditRouter = require('express').Router();
 // Helpers
 const {
@@ -7,14 +9,16 @@ const {
 } = require('../global/globalHelpers');
 // Endpoints
 redditRouter.get('/', requireLogin, (req, res, next) => {
-  res.status(200).json({ url: authorizeRedditAccess() });
+  const { mobile } = req.query;
+  res.status(200).json({ url: authorizeRedditAccess(mobile ? true : false) });
 });
 
 redditRouter.get('/auth', requireLogin, (req, res, next) => {
   const { state, code, error } = req.query;
-  const isValidState = bcrypt.compareSync(config.redditState, state);
+  const isValidState = bcrypt.compareSync(config.redditState, decodeURIComponent(state));
   if(isValidState && code && !error) {
     res.status(200).json({ authorized: true });
+    // get the access token next
   } else {
     next({ message: error, status: 403 });
   }
