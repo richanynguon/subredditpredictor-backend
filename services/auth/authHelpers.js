8d628a66ff4f38ccd1c9e655add52e14a28cdf1d
Melquisedeque Pereira <melqui1@live.com.pt>
2019-11-18 16:36:14 +0000

    Fixed name of method to findUser

--- services/auth/authHelpers.js
+++ services/auth/authHelpers.js
@@ -39,11 +39,11 @@ const findUser = (filter) => {
 }
 
 const addUser = (newUser) => {
-  return db(users).insert(newUser).then((ids) => findOne({ id: ids[0] }));
+  return db(users).insert(newUser).then((ids) => findUser({ id: ids[0] }));
 }
 
 const updateUser = (changes, id) => {
-  return db(users).where({ id }).update(changes).then(() => findOne({ id }));
+  return db(users).where({ id }).update(changes).then(() => findUser({ id }));
 }
 
 const removeUser = (id) => {
