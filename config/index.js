e5afc920391034bc836efd692da9a84eae8e17ff
Melquisedeque Pereira <melqui1@live.com.pt>
2019-11-18 16:37:10 +0000

    Added required configuration for reddit api

--- config/index.js
+++ config/index.js
@@ -12,4 +12,9 @@ module.exports = {
     'http://localhost:3000',
   ],
   secure: isProduction, // cookie sent over http only in dev
+  jwtSecret: process.env.TOKENSECRET,
+  redditState: process.env.REDDIT_STATE,
+  redditClientId: process.env.REDDIT_CLIENT_ID,
+  redditClientSecret: process.env.REDDIT_CLIENT_SECRET,
+  redditRedirectURL: process.env.REDDIT_REDIRECT_URL
 }
